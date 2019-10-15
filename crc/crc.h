703780c7578aee1d307e9c787741c9118cc3c27c
刘振国 <184228676@qq.com>
2019-10-15 23:44:33 +0800

    添加了常用的CRC16校验函数，还未测试

--- crc/crc.h
+++ crc/crc.h
@@ -1,1 +1,12 @@
 #pragma once
+void InvertUint8(unsigned char* DesBuf, unsigned char* SrcBuf);
+void InvertUint16(unsigned short* DesBuf, unsigned short* SrcBuf);
+unsigned short CRC16_CCITT(unsigned char* puchMsg, unsigned int usDataLen);
+unsigned short CRC16_CCITT_FALSE(unsigned char* puchMsg, unsigned int usDataLen);
+unsigned short CRC16_XMODEM(unsigned char* puchMsg, unsigned int usDataLen);
+unsigned short CRC16_X25(unsigned char* puchMsg, unsigned int usDataLen);
+unsigned short CRC16_MODBUS(unsigned char* puchMsg, unsigned int usDataLen);
+unsigned short CRC16_IBM(unsigned char* puchMsg, unsigned int usDataLen);
+unsigned short CRC16_MAXIM(unsigned char* puchMsg, unsigned int usDataLen);
+unsigned short CRC16_USB(unsigned char* puchMsg, unsigned int usDataLen);
+
